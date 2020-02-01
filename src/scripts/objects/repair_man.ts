b2609ed862386756ce894f2cd502ef47946adb3e
Sushanth15 <sushanth_15@outlook.com>
2020-02-01 18:54:23 +0530

    DragRepairMen

--- src/scripts/objects/repair_man.ts
+++ src/scripts/objects/repair_man.ts
@@ -3,14 +3,22 @@ export default class RepairMan extends Phaser.GameObjects.Image {
 
     public skill: string;
     public time: number;
+    private image : any;
 
 
     constructor(scene: Phaser.Scene, x: number, y: number, key: string) {
+
         super(scene, x, y, key);
+        this.texture.getSourceImage();
         this.setOrigin(0.5, 0.5);
+     
     }
 
     update(time, dt) {
 
     }
+
+ 
+
+    
 }
