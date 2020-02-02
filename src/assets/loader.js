4f1f416fd0e33ef674d0194632e32a8a57f6c1f6
Sushanth15 <sushanth_15@outlook.com>
2020-02-02 16:13:44 +0530

    stats and opacity tween

--- src/assets/loader.js
+++ src/assets/loader.js
@@ -11,6 +11,10 @@ import computer_repair from  './images/computer_repair.png';
 import room_electrician from './images/room_electrician.png';
 import room_carpenter from './images/room_carpenter.png';
 import room_computer_repair from  './images/room_computer_repair.png';
+import icon_electrician from './images/icon_electrician.png';
+import icon_carpenter from './images/icon_carpenter.png';
+import icon_computer_repair from  './images/icon_computer_repair.png';
+
 
 export {
     
@@ -24,6 +28,9 @@ export {
     room_electrician,
     room_carpenter,
     room_computer_repair,
+    icon_electrician,
+    icon_carpenter,
+    icon_computer_repair,
     font1_data,
     font1_texture
 };
